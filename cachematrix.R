f2b65c20a2d697a2ddb274a768dcc0094818d66d
Alejandra Ugarte <Alejandra_Ugarte@outlook.com>
2017-07-22 00:14:31 +0100

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,16 @@
 ## The following function 'makeCacheMatrix' creates a special "matrix" object 
-## that can cache the inverse of the matrix.The matrix must be a square as the
-## solve() function in the 2nd function(see below) can only compute square 
-##matrices.
+## that can cache the inverse of the matrix.
+
+## 'x' and 'a' are first initialised as objects within the function, 'a' is set to NULL
+## so that it can be used later. The default value of 'x' is an empty matrix.
+## The 'set' function assigns the 'y' input to 'x' in the parent environment and 'a' to NULL. 
+## This clears the cached value of 'a' if you have already used the function. 
+## The 'get' function retrieves 'x' from the parent environment of makeCacheMatrix.
+## 'setinverse' assigns input argument to the value of 'a' in parent environment.
+## 'getinverse' defines the retrieval of the inverse 'a'
+## The list defines each function in the form of a list and returns it to makeCacheMatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-  
   a <- NULL
   set <- function(y){
     x <<- y
@@ -26,6 +32,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## The 'cacheSolve' function computes the inverse of the 'special' matrix that was
 ## cached in the 'makeCacheMatrix'.
+
+## 'cacheSolve' is given the argument 'x' and '...' 
+## The function defines 'a' which tries to getinverse() on the object that was passed as the argument.
+## If 'a' is not NULL it will return the inverse of the cached matrix.
+## If 'a' is NULL it gets the matrix from the input object and calculates the inverse by using 'setinverse'
+## It then returns the inverse by printing the object 'a'
 
 cacheSolve <- function(x, ...) {
   a <- x$getinverse()
