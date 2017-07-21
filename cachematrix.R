7f8591950297c601194b3661f999127933987b3f
Alejandra Ugarte <Alejandra_Ugarte@outlook.com>
2017-07-21 22:55:51 +0100

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following function 'makeCacheMatrix' creates a special "matrix" object 
+## that can cache the inverse of the matrix.The matrix must be a square as the
+## solve() function in the 2nd function(see below) can only compute square 
+##matrices.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  a <- NULL
+  set <- function(y){
+    x <<- y
+    a <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(inverse) a <<- inverse
+  
+  getinverse <- function() a
+  
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve' function computes the inverse of the 'special' matrix that was
+## cached in the 'makeCacheMatrix'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  a <- x$getinverse()
+  if(!is.null(a)){
+    message("getting cached data")
+    return(a)
+  }
+  data <- x$get()
+  a <- solve(data,...)
+  x$setinverse(a)
+  a
+  ## Returns a matrix that is the inverse of 'x'
 }
+
+#testing the function, should return an inverse of a 5 by 5 square matrix
+
+f <- matrix(rnorm(25),5,5)
+f1 <- makeCacheMatrix(f)
+cacheSolve(f1)
